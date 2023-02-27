7d0d07f1508a14cd56b400279e34351e941758ac
Kevyn Gorito Germano <kevyng@tecgraf.puc-rio.br>
2023-02-27 16:34:28 -0300

    Abordando compartilhamento entre componentes

---
+++ myLearning/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule{
+
+  
+
+ }
